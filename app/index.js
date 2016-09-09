e150fd4349010254e59c1f45dda1adbab9a9b591
oleg-ns <oleg-ns@bk.ru>
2016-09-09 09:38:28 +0300

    2

--- app/index.js
+++ app/index.js
@@ -4,3 +4,41 @@ $('#modalLogIn').on('shown.bs.modal', function () {
 $('#modalSignIn').on('shown.bs.modal', function () {
     $('#myInput').focus()
 })
+
+$('img').on("mouseover", function () {
+    var target = $( event.target );
+    if (target.is('#barcelona')) {
+        $('.barcelonaAbout').css({
+            "display": "block"
+        })
+        }
+    if (target.is('#paris')) {
+        $('.parisAbout').css({
+            "display": "block"
+        })
+    }
+    if (target.is('#kiev')) {
+        $('.kievAbout').css({
+            "display": "block"
+        })
+    }
+    })
+$('img').on("mouseout", function () {
+    var target = $( event.target );
+    if (target.is('#barcelona')) {
+        $('.barcelonaAbout').css({
+            "display": "none"
+        })
+    }
+    if (target.is('#paris')) {
+        $('.parisAbout').css({
+            "display": "none"
+        })
+    }
+    if (target.is('#kiev')) {
+        $('.kievAbout').css({
+            "display": "none"
+        })
+    }
+})
+
